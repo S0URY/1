71280a0918bdbf7ac631d61e45cd8b6d769933c2
S0URY <yesntnoe@gmail.com>
2023-08-09 14:17:09 +0700

    innit

---
+++ src/Day1yey.java
@@ -0,0 +1,18 @@
+
+public class Day1yey {
+    public static void main(String[] args) {
+
+        System.out.println("---Data Mahasiswa---");
+        System.out.println("Nama: Hadley Seabert. H");
+        System.out.println("NRP: c14230083");
+        System.out.println("Mata Kuliah: Algoritma dan Pemrograman");
+        System.out.println("Kelas: B");
+        System.out.println("Tahun: 2023/2024");
+        System.out.println("Semester: Gasal");
+        System.out.println("--------------------");
+
+
+
+    }
+    }
+
---
+++ src/aaaaaaaaaaaaa.java
@@ -0,0 +1,20 @@
+
+import java.lang.annotation.Repeatable;
+import java.util.Random;
+
+public class aaaaaaaaaaaaa {
+    public static void main(String[] args) {
+        Random random = new Random();
+        for (int i = 0; i < 6; i++) {
+        int angka = random.nextInt(101) + 1;
+
+        System.out.println(angka);
+            if (angka >=1 && angka <= 50){
+                System.out.println("Rush E");
+            } else {
+                System.out.println("Rush B");
+
+            }
+        }
+    }
+}
